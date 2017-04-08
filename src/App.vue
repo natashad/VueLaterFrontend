4f49595093525b09ca0f2a039788b77f31602cc6
Natasha Dalal <natashadalal@gmail.com>
2017-04-08 18:45:17 -0400

    Initial comimt

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <app-header></app-header>
+  <app-sharer></app-sharer>
+  <app-items></app-items>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Sharer from './components/Sharer.vue';
+import Header from './components/items/ItemContainer.vue';
+
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+    'appHeader': Header,
+    'appSharer': Sharer,
+    'appItems': Items,
+  }
+}
+</script>
+
+<style>
+
+</style>
